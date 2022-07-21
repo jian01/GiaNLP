4e0ba4a8dbf8e072d3cedeb1cb3ecc15d80e5f1e
Gianmarco Cafferata <giancafferata@hotmail.com>
2022-07-21 00:47:47 -0300

    Project site and description

--- setup.py
+++ setup.py
@@ -1,8 +1,13 @@
 """
 Library setup
 """
-
+import sys
+from os import path
 from setuptools import find_packages, setup  # type: ignore
+
+readme_file = path.join(path.dirname(path.abspath(__file__)), "README.md")
+with open(readme_file) as f:
+    readme = f.read()
 
 # Library dependencies
 INSTALL_REQUIRES = ["gensim>=4.0.0", "pandas", "tqdm"]
@@ -14,14 +19,17 @@ setup(
     name="GiaNLP",
     version="0.0.1",
     description="Natural Language Processing for humans",
+    long_description=readme,
+    long_description_content_type="text/markdown",
     author="Gianmarco Cafferata",
     author_email="gcafferata@fi.uba.ar",
-    url="https://github.com/jian01/GiaNLP",
+    url="https://jian01.github.io/GiaNLP/",
     packages=["gianlp"],
     python_requires=">=3.5",
     setup_requires=["wheel"],
     install_requires=INSTALL_REQUIRES,
-    extras_require={"test": TEST_REQUIRES},
+    tests_require=TEST_REQUIRES,
+    test_suite="tests",
     classifiers=[
         "Environment :: Web Environment",
         "Intended Audience :: Developers",
@@ -30,6 +38,9 @@ setup(
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         "Topic :: Software Development",
     ],
 )
