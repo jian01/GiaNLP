7c85f837608ec0ca0c52ba7c932e4622b7188346
Gianmarco Cafferata <giancafferata@hotmail.com>
2022-08-06 15:26:19 -0300

    Change test matrix

--- noxfile.py
+++ noxfile.py
@@ -5,7 +5,7 @@ import nox
 
 
 @nox.session(
-    python="3.7.0"
+    python="3.8"
 )
 def tests_tensorflow_23(session):
     """Run all tests."""
@@ -89,7 +89,7 @@ def tests_tensorflow_28(session):
     session.run(*cmd)
 
 @nox.session(
-    python="3.9"
+    python="3.10"
 )
 def tests_tensorflow_latest(session):
     """Run all tests."""
